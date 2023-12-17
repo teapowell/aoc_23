a1b8896a1c211ac8f03d5cede24f6788d14bc900
Tim Powell <tim.powell@stfc.ac.uk>
2023-12-17 22:56:33 +0000

    day 2 task 1

--- aoc_23.py
+++ aoc_23.py
@@ -1,3 +1,6 @@
+import re
+
+
 def day1_1(input):
     line_values = []
     with open(input) as file:
@@ -47,10 +50,51 @@ def day1_2(input):
     print("  Answer: " + str(day1_2_result))
 
 
+def day2_1(input):
+    red_cubes = 12
+    green_cubes = 13
+    blue_cubes = 14
+    poss_games = []
+
+    # open file
+    with open(input) as file:
+        lines = [line.strip() for line in file]
+
+    for line in lines:
+        # initiate game colour dict
+        game_col_dict = {"red": [], "green": [], "blue": []}
+        # split string
+        spl_line = re.split(": |, |; | ", line)
+        # get only colour info
+        game_col = spl_line[2:]
+        # get all values of cubes pulled
+        for i in range(0, len(game_col), 2):
+            game_col_dict[game_col[i + 1]].append(int(game_col[i]))
+        # get max value of cubes pulled
+        game_col_dict["red"] = max(game_col_dict["red"])
+        game_col_dict["green"] = max(game_col_dict["green"])
+        game_col_dict["blue"] = max(game_col_dict["blue"])
+        # check if possible game
+        if (
+            game_col_dict["red"] <= red_cubes
+            and game_col_dict["green"] <= green_cubes
+            and game_col_dict["blue"] <= blue_cubes
+        ):
+            poss_games.append(int(spl_line[1]))
+
+    #     print(spl_line[1], game_col_dict)
+    # print(poss_games)
+
+    day2_1_result = sum(poss_games)
+    print("\n  Day 2 - Task 1")
+    print("  Answer: " + str(day2_1_result))
+
+
 def main():
     print("\n~~~ Advent Of Code 2023 ~~~\n")
     day1_1("/Users/tim.powell/Documents/vcs/aoc_23/inputs/day1.txt")
     day1_2("/Users/tim.powell/Documents/vcs/aoc_23/inputs/day1.txt")
+    day2_1("/Users/tim.powell/Documents/vcs/aoc_23/inputs/day2_1.txt")
 
 
 main()
